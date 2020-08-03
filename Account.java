1f91b2e0d47eed2e721918a9e1d256e9116a30ce
MdevSC <58511808+MdevSC@users.noreply.github.com>
2020-08-03 18:28:32 +0100

    New Account

---
+++ Account.java
@@ -0,0 +1,27 @@
+package newbank.server;
+import java.util.Arrays;
+
+public class Account {
+	
+	private String accountName;
+	private double openingBalance;
+	public static String[] accountArray = {"Main","Checking","Savings"};
+
+	public Account(String accountName, double openingBalance) {
+		this.accountName = accountName;
+		this.openingBalance = openingBalance;
+
+	}
+
+	public void Deposit(String accountName,double amount){
+		if (amount >0){
+			this.openingBalance +=amount;
+		}
+
+	}
+	
+	public String toString() {
+		return (accountName + ": " + openingBalance);
+	}
+
+}
